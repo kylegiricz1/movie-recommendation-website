b757046517781d704ea2b883bed8e71bc3e08e62
John Doan <123352009+johndoans@users.noreply.github.com>
2025-10-02 22:44:51 -0500

    Major changes for CSV parsing and table beautification

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -3,39 +3,47 @@ import React, { useEffect, useState } from 'react';
 function Table() {
     const [data, setData] = useState([]);
 
+    // Get back-end URL
+    const URL = window.location.href.replace("3000", "5000") + "/data";
+
+    // Fetch the data from the back-end
     useEffect(() => {
-        fetch("http://localhost:5000/data")
+        fetch(URL)
         .then(response => response.json())
         .then(json => setData(json))
         .catch(error => console.error("Something went wrong! \n Error: ", error));
     }, []);
 
+    // Show loading text in meantime
     if (data.length === 0) {
         return <p>Loading...</p>;
     }
 
-    const headers = Object.keys(data[0])
-
     return (
-        <table border="1" cellPadding="5">
+        <table border="2px solid" cellPadding="5">
             <thead>
-                <tr>
-                    {
-                        headers.map((header, position) => (
-                            <th key={position}>{header}</th>
-                        ))
-                    }
+                <tr style={{ backgroundColor: "#f2f2f2"}}>
+                    <th>Name</th>
+                    <th>Year</th>
+                    <th>Genres</th>
+                    <th>Popularity</th>
+                    <th>Vote Average</th>
+                    <th>Vote Count</th>
+                    <th>Budget</th>
                 </tr>
             </thead>
             <tbody>
                 {
+                    // The different columns of the table
                     data.map((row, position) => (
                         <tr key={position}>
-                            {
-                                headers.map((header, i) => (
-                                    <td key={i}>{row[header]}</td>
-                                ))
-                            }
+                            <td key={position}>{row.title}</td>
+                            <td key={position}>{row.release_date}</td>
+                            <td key={position}>{JSON.parse(row.genres).map(item => item.name).join(", ")}</td>
+                            <td key={position}>{row.popularity}</td>
+                            <td key={position}>{row.vote_average}/10</td>
+                            <td key={position}>{row.vote_count}</td>
+                            <td key={position}>${row.budget}</td>
                         </tr>
                     ))
                 }
