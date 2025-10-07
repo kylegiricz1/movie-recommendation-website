cfbf96b9e91d01cc0f6e254b8712d5426987d0ea
Mikey8 Bud <mikeybud08@gmail.com>
2025-10-07 14:56:13 -0500

    Added year selection to frontend.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,10 +3,12 @@ import Table from "../components/Table.js";
 
 function HomePage() {
   const [genre, setGenre] = useState("");
+  const [year, setYear] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevent page reload
-    console.log("Selected Genre:", genre);
+  console.log("Selected Genre:", genre);
+  console.log("Selected year:", year);
     // Call your API or filter movies here
   };
 
@@ -21,6 +23,15 @@ function HomePage() {
             name="genre"
             value={genre}
             onChange={(e) => setGenre(e.target.value)}
+          />
+        </label>
+        <label style={{ marginLeft: '12px' }}>
+          Release Year:
+          <input
+            type="number"
+            name="year"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
           />
         </label>
         <button type="submit">Submit</button>
