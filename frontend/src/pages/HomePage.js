8299a772fed1f7117a8a8e78e27bd92ca65c3421
John Doan <123352009+johndoans@users.noreply.github.com>
2025-10-21 03:42:58 +0000

    Made basic filter U.I. changes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -17,24 +17,81 @@ function HomePage() {
     <div>
       <Navbar/>
       <form onSubmit={handleSubmit}>
+        Genres:
+        <br></br>
         <label>
-          Genre:
-          <input
-            type="text"
-            name="genre"
-            value={genre}
-            onChange={(e) => setGenre(e.target.value)}
-          />
+          <input type="checkbox" name="action" />Action
         </label>
-        <button type="submit">Submit</button>
+        <br></br>
+        <label>
+          <input type="checkbox" name="adventure" />Adventure
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="animation" />Animation
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="comedy" />Comedy
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="crime" />Crime
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="documentary" />Documentary
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="drama" />Drama
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="family" />Family
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="fantasy" />Fantasy
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="history" />History
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="horror" />Horror
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="mystery" />Mystery
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="romance" />Romance
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="science-fiction" />Science Fiction
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="thriller" />Thriller
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="war" />War
+        </label>
+        <br></br>
+        <label>
+          <input type="checkbox" name="western" />Western
+        </label>
+        <br></br>
         <label style={{ marginLeft: '12px' }}>
           Release Year:
-          <input
-            type="number"  
-            name="year"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
+          &nbsp;
+          <input type="date" name="year" value={year} onChange={(e) => setYear(e.target.value)}/>
+          &nbsp;
         </label>
         <button type="submit">Submit</button>
       </form>
