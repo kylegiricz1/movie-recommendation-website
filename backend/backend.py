9d87fe03b6af43df981b069c7ba444b7bcd80d9b
Mikey8 Bud <mikeybud08@gmail.com>
2025-10-25 22:51:59 -0500

    Updated

--- backend/backend.py
+++ backend/backend.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import csv
 
+# Install Flask using pip install Flask (MAC "pip install Flask") 
+# # Run this file using python backend.py (MAC "python3 backend.py") 
+# # Access the server at http://localhost:5000/ <-example
 app = Flask(__name__)
 CORS(app)
 
@@ -14,17 +17,20 @@ def get_data():
     data = []
 
     try:
-        # ✅ Read CSV using UTF-8 so Windows doesn't break
+        #Read CSV using UTF-8 so Windows doesn't break
         with open("tmdb_5000_movies.csv", encoding="utf-8") as file:
             reader = csv.DictReader(file)
 
+            # Add each row to list
             for row in reader:
                 data.append(row)
 
     except Exception as e:
+        # Send error message back to user
         print(f"Something went wrong! \nError: {str(e)}")
         return {"error": str(e)}
 
+   # Return as JSON dictionary
     return jsonify(data)
 
 if __name__ == '__main__':
