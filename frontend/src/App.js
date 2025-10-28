a6c9840de2792f51701912493d4a6ba83e796734
kylegiricz1 <159865309+kylegiricz1@users.noreply.github.com>
2025-10-28 10:45:34 -0500

    added basic styling to about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 // Import other pages/components as needed
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
