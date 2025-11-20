7601269be6885254f3e27894ae10c82c4337eaf3
John Doan <123352009+johndoans@users.noreply.github.com>
2025-11-19 20:13:19 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Recommendations from "./pages/Recommendations";
 import Wizard from "./pages/Wizard";
 import List from "./pages/List";
 // Import other pages/components as needed
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/list" element={<List />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/recommendations" element={<Recommendations />} />
         <Route path="/wizard" element={<Wizard />} />
       </Routes>
     </Router>
